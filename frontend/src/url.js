8976fee150b463064d9fe8073674be1bdf7f133c
Snehasish-Dey <snehasishdey333@gmail.com>
2023-06-27 23:46:43 +0530

    added links

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,4 @@
-// export const URL=import.meta.env.VITE_URL
-// export const PF=import.meta.env.VITE_PF
-export const URL="https://blog-market.onrender.com"
-export const PF="https://blog-market.onrender.com/images/"
+export const URL=import.meta.env.VITE_URL
+export const PF=import.meta.env.VITE_PF
+
 // export default URL
